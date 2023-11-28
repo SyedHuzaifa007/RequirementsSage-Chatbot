bd26be050f2ca94ddc004a660efe0a2e63e5c779
AdolfTrumpler <116947935+AdolfTrumpler@users.noreply.github.com>
2023-11-28 05:30:51 +0500

    e

--- xhatapp/Config.py
+++ xhatapp/Config.py
@@ -8,7 +8,7 @@ import openai
 class BravoSis:
     def __init__(self):
         self.openai_api_key = "" # set your own api value
-        self.openai_api_key = "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" #get this value from https://beta.openai.com/.
+        self.openai_api_key = "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" #get this value from https://beta.openai.com/.
         self.model = "text-davinci-003" # use any of these [text-davinci-002,text-davinci-001]
         self.mxtoken = 1080 #can decrese/increse with reaspect to result previlage
 
